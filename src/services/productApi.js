87c2cbc0d429b1ec7415d19577e0b48c94e1c28e
vighnesh1310 <vighneshkadam575@gmail.com>
2026-09-23 21:14:34 +0530

    feat: add product API services

---
+++ src/services/productApi.js
@@ -0,0 +1,60 @@
+import api from "./axios";
+
+export const getProducts = async (limit = 10, skip = 0) => {
+  const response = await api.get("/products", {
+    params: {
+      limit,
+      skip,
+    },
+  });
+
+  return response.data;
+};
+
+export const searchProducts = async (
+  query,
+  limit = 10,
+  skip = 0,
+  signal
+) => {
+  const response = await api.get("/products/search", {
+    params: {
+      q: query,
+      limit,
+      skip,
+    },
+    signal,
+  });
+
+  return response.data;
+};
+
+export const getCategories = async () => {
+  const response = await api.get("/products/categories");
+
+  return response.data;
+};
+
+export const getProductById = async (id) => {
+  const response = await api.get(`/products/${id}`);
+
+  return response.data;
+};
+
+export const addProduct = async (product) => {
+  const response = await api.post("/products/add", product);
+
+  return response.data;
+};
+
+export const updateProduct = async (id, product) => {
+  const response = await api.put(`/products/${id}`, product);
+
+  return response.data;
+};
+
+export const deleteProduct = async (id) => {
+  const response = await api.delete(`/products/${id}`);
+
+  return response.data;
+};
